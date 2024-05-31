50e302379c3622a663ebfcca44a66db493970e15
EslamElnajdy <eslammhamed425@gmail.com>
2024-06-01 00:55:36 +0300

    task 0

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,42 @@
+import App from './App';
+import { shallow } from 'enzyme';
+import React from 'react';
+
+describe('App component', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders a div with the notifications', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Notifications').length).toBe(1);
+  });
+
+  it('renders a div with the header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Header').length).toBe(1);
+  });
+
+  it('renders a div with the login', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Login').length).toBe(1);
+  });
+
+  it('renders a div with the footer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Footer').length).toBe(1);
+  });
+
+  it('check that CourseList is not displayed', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('CourseList').length).toBe(0);
+    expect(wrapper.find('Login').length).toBe(1);
+  });
+
+  it('check that login is not displayed', () => {
+    const wrapper = shallow(<App isLoggedIn={true} />);
+    expect(wrapper.find('Login').length).toBe(0);
+    expect(wrapper.find('CourseList').length).toBe(1);
+  });
+});
