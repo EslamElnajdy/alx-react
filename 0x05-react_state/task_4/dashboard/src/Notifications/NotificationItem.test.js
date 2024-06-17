0848d1cdc8e2ca27faa2e668d8e7c029315087fa
EslamElnajdy <eslammhamed425@gmail.com>
2024-06-17 13:56:24 +0300

    task 4

---
+++ 0x05-react_state/task_4/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { shallow } from "enzyme";
+import NotificationItem from "./NotificationItem";
+
+describe("test the NotificationItem component", () => {
+  it("renders without crashing", () => {
+    const markAsReadMock = jest.fn();
+    const wrapper = shallow(<NotificationItem id={1} markAsRead={markAsReadMock} />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it("render the correct html", () => {
+    const markAsReadMock = jest.fn();
+    const wrapper = shallow(<NotificationItem id={1} type="default" value="test" markAsRead={markAsReadMock} />);
+    expect(wrapper.prop('data-notification-type')).toEqual('default');
+    expect(wrapper.text()).toBe('test');
+  });
+
+  it("renders the correct html with html prop", () => {
+    const markAsReadMock = jest.fn();
+    const htmlContent = { __html: "<u>test</u>" };
+    const wrapper = shallow(<NotificationItem id={1} type="default" html={htmlContent} markAsRead={markAsReadMock} />);
+    expect(wrapper.prop('data-notification-type')).toEqual('default');
+    expect(wrapper.html()).toContain('<u>test</u>');
+  });
+
+  it('calls markAsRead on click', () => {
+    const markAsReadMock = jest.fn();
+    const wrapper = shallow(<NotificationItem id={1} type="default" value="test" markAsRead={markAsReadMock}/>);
+    wrapper.find('li').simulate('click');
+    expect(markAsReadMock).toHaveBeenCalledWith(1);
+  });
+})
